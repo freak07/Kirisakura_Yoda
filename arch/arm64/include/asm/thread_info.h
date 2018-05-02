f62832fa3d99b4b5acd1762c77f334cfeedbfe74
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 11:05:22 -0700

    BACKPORT: FROMLIST: arm64: implement Shadow Call Stack
    This change implements shadow stack switching, initial SCS set-up,
    and interrupt shadow stacks for arm64.
    
    Bug: 145210207
    Change-Id: I3d5b9ec374418b110d1f351e1abd41610cfee597
    (am from https://lore.kernel.org/patchwork/patch/1149062/)
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    (cherry picked from commit 98308264140092420b14475efd1d2721546c51d1)

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -47,6 +47,10 @@ struct thread_info {
        struct mutex *          pWaitingMutex;          //ASUS_BSP + [thomas]Add for slow log
        struct completion *     pWaitingCompletion;     //ASUS_BSP + [thomas]Add for slow log
        struct rt_mutex *       pWaitingRTMutex;        //ASUS_BSP + [thomas]Add for slow log
+
+#ifdef CONFIG_SHADOW_CALL_STACK
+	void			*shadow_call_stack;
+#endif
 };
 
 #define INIT_THREAD_INFO(tsk)						\
