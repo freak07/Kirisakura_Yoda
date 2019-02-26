4894669b6f5492f3e52656a05c499c302bd0e3d3
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2019-02-26 09:38:09 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Ie37ab752a4d69569bce506b0a12715bb45ece79e
    Co-developed-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    (cherry picked from commit 5c81c0c3bcd016fa32dd4f0bdd115fa1fb244f48)
    (cherry picked from commit c4385e322af65827ca95c81a48362904e3d32f2c)

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -429,6 +429,15 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two_million,
 	},
 	{
+		.procname       = "sched_asym_cap_sibling_freq_match_pct",
+		.data           = &sysctl_sched_asym_cap_sibling_freq_match_pct,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1         = &one,
+		.extra2         = &one_hundred,
+	},
+	{
 		.procname	= "sched_prefer_spread",
 		.data		= &sysctl_sched_prefer_spread,
 		.maxlen		= sizeof(unsigned int),
