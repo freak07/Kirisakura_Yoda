3dab2d8d7fecd596d67eee0c72faa6ac95f3ccbb
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-09-03 15:00:25 +0530

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I310bbdc19bb65a0c562ec6a208f2da713eba954d
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    (cherry picked from commit e193cefc365d6b667eceecb6c20996bfdc335a71)

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -34,6 +34,7 @@ extern unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS];
 extern unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS];
 #ifdef CONFIG_SCHED_WALT
 extern unsigned int sysctl_sched_user_hint;
+extern const int sched_user_hint_max;
 extern unsigned int sysctl_sched_use_walt_cpu_util;
 extern unsigned int sysctl_sched_use_walt_task_util;
 extern unsigned int sysctl_sched_walt_init_task_load_pct;
