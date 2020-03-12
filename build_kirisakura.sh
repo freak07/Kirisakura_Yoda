8c7dfea177734356049caab2c693a33871010e1b
freak07 <michalechner92@googlemail.com>
2020-03-12 10:17:45 +0100

    buildscript: adapt for recent changes

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -13,18 +13,20 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983c/bin
+export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r377782b/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r377782b/lib64:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
 echo 
-make CC=clang O=out kirisakura_defconfig
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out kirisakura_defconfig
 
 echo
 echo "Build The Good Stuff"
 echo 
 
-make CC=clang O=out -j4
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j4
