freak07/OP7PRO

f2fs: inode.c: account for recent f2fs stable merge
(cherry picked from commit 806fb3967e7ebb7cf2de27b08ca21b1b39c60c5d)
(cherry picked from commit 1ccf1fe2c9cb80be74bfdbffe681e2471f449915)

fs/f2fs/inode.c
@@ -705,9 +705,8 @@ no_delete:
 
 	if (unlikely(is_inode_flag_set(inode, FI_DIRTY_INODE))) {
 		f2fs_inode_synced(inode);
-		f2fs_msg(sbi->sb, KERN_WARNING,
-			 "inconsistent dirty inode:%u entry found during eviction\n",
-			 inode->i_ino);
+		f2fs_warn(sbi, "inconsistent dirty inode:%u entry found during eviction\n",
+		inode->i_ino);
 		if (!is_set_ckpt_flags(sbi, CP_ERROR_FLAG) &&
 		    !is_sbi_flag_set(sbi, SBI_CP_DISABLED))
 			f2fs_bug_on(sbi, 1);

freak07/Kirisakura_Yoda

(cherry picked from commit c5c424b3e063da73cf7f34daadd430e296ae29dc)

fs/f2fs/inode.c
@@ -705,9 +705,8 @@ no_delete:
 
 	if (unlikely(is_inode_flag_set(inode, FI_DIRTY_INODE))) {
 		f2fs_inode_synced(inode);
-		f2fs_msg(sbi->sb, KERN_WARNING,
-			 "inconsistent dirty inode:%lu entry found during eviction\n",
-			 inode->i_ino);
+		f2fs_warn(sbi, "inconsistent dirty inode:%lu entry found during eviction\n",
+		inode->i_ino);
 		if (!is_set_ckpt_flags(sbi, CP_ERROR_FLAG) &&
 		    !is_sbi_flag_set(sbi, SBI_CP_DISABLED))
 			f2fs_bug_on(sbi, 1);