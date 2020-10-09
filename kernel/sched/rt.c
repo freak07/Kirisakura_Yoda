8725206c67054f3a90026045180db2942f9371b8
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-10-09 12:02:18 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvemnts.
    
    Change-Id: I8e6d7afe12387d86f3721c6fd880812ba86d885e
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    (cherry picked from commit e3a0b52bb2116950f15ceb9d205ef8d8c5b0f0d7)
    (cherry picked from commit 5786cd7c1e89f14254d7843de0a23c71a225be93)
    (cherry picked from commit 6e29b3b5651eacf7533e19c01cca9168903f2911)
    (cherry picked from commit a47471992a046c720be486893d164fac8d16952a)
    (cherry picked from commit 82faf6c3e28dad3530c8769b8a033e0097366753)
    (cherry picked from commit 6e341e833a508501effbf22eabf67ac1d6cba63f)
    (cherry picked from commit 63941d1efd78f9179cab91304782c1e790793169)

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1778,6 +1778,7 @@ static int rt_energy_aware_wake_cpu(struct task_struct *task)
 	bool boost_on_big = sched_boost() == FULL_THROTTLE_BOOST ?
 				  (sched_boost_policy() == SCHED_BOOST_ON_BIG) :
 				  false;
+	bool best_cpu_lt = true;
 
 	rcu_read_lock();
 
@@ -1804,6 +1805,8 @@ retry:
 		}
 
 		for_each_cpu_and(cpu, lowest_mask, sched_group_span(sg)) {
+			bool lt;
+
 
 			trace_sched_cpu_util(cpu);
 
@@ -1818,8 +1821,20 @@ retry:
 
 			util = cpu_util(cpu);
 
-			/* Find the least loaded CPU */
-			if (util > best_cpu_util)
+			lt = (walt_low_latency_task(cpu_rq(cpu)->curr) ||
+				walt_nr_rtg_high_prio(cpu));
+
+			/*
+			 * When the best is suitable and the current is not,
+			 * skip it
+			 */
+			if (lt && !best_cpu_lt)
+				continue;
+			/*
+			 * Either both are sutilable or unsuitable, load takes
+			 * precedence.
+			 */
+			if (!(best_cpu_lt ^ lt) && (util > best_cpu_util))
 				continue;
 
 			/*
@@ -1854,6 +1869,7 @@ retry:
 			best_cpu_util = util;
 			best_cpu = cpu;
 			best_capacity = capacity_orig;
+			best_cpu_lt = lt;
 		}
 
 	} while (sg = sg->next, sg != sd->groups);
